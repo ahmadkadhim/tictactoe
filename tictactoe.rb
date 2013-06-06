b4c7939240ea933f7d2e9ce35002e4b813d34a32
AhmadKadhim <akadhim13@gmail.com>
2013-06-06 08:55:13 -0400

    near final version, minor issues

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,4 +1,79 @@
-require_relative "./tictactoe_runner"
+class Tic_tac_toe
 
-class tictactoe
+	attr_accessor :counter, :current_player
 
+	class Already_Chosen_Exception < Exception
+	end
+
+	class Invalid_Choice_Exception < Exception
+	end
+
+	def initialize
+		@board = [[1,2,3],[4,5,6],[7,8,9]]
+		@player_one = []
+		@player_two = []
+		@all_choices = []
+		@current_player = ""
+		@counter = 1
+		@winning_combos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+	end
+
+	def draw_board
+		puts ""
+		@board.each { |x| puts x.join(" ").rjust(20) }
+		puts ""
+	end 
+
+	def question_player(player)
+		puts "#{@current_player}: what's your choice?"
+		@choice = gets.chomp.to_i
+		pick_finder(@choice)
+		begin
+			error_check
+		rescue Invalid_Choice_Exception => e
+			puts e.message
+			puts ""
+			@counter -= 1
+		rescue Already_Chosen_Exception => e
+			puts e.message
+			puts ""
+			@counter -= 1
+		end
+		@all_choices.push(@choice)
+		player.push(@choice)
+		puts "#{@current_player} has guessed: #{player.join(", ")}"
+	end
+
+	def win?
+		@winning_combos.each_with_index do | val, index |
+			if (@winning_combos[index] - whose_turn).empty?			
+				return true
+			end
+		end
+		return false
+	end
+
+	def whose_turn
+		@counter.odd? ? @current_player = "PLAYER ONE" : @current_player = "PLAYER TWO"
+		@counter.odd? ? @player_one : @player_two
+	end
+
+	def error_check
+		raise Invalid_Choice_Exception.new("invalid choice") if @choice.to_s.length != 1 # ||  @choice.include?(/^1-9/)
+		raise Already_Chosen_Exception.new("That's already been chosen! Try again!") if @all_choices.include?(@choice) 
+	end
+
+	def pick_finder(pick)
+		@board.each_with_index do |val,index|
+			outer_index = index
+			val.each_with_index do |val,index|
+				if pick == val
+					@counter.odd? ? @board[outer_index][index] = "x" : @board[outer_index][index] = "o"
+				end			
+			end
+		end
+		draw_board
+		p "***************************************"
+	end
+
+end
--- tictactoe_runner.rb
+++ tictactoe_runner.rb
@@ -1,1 +1,23 @@
-class tictactoe_runner
+require "./tictactoe.rb"
+
+class Tictactoe_runner
+
+	def self.run
+		@game = Tic_tac_toe.new
+		@game.draw_board
+		until @game.counter == 10
+			@game.question_player(@game.whose_turn)
+			if @game.win?
+				puts ""
+				puts " * * * #{@game.current_player} wins! * * * ".rjust(20)
+				puts ""
+				break
+			end
+			@game.counter += 1	
+		end
+	p "draw game . . ." if @game.counter == 10
+	end
+
+end
+
+Tictactoe_runner.run
